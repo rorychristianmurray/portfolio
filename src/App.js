aa8e5b40a58c554af7f1ccbc4ea4e2982647a6de
Rory Murray <rory.christian.murray@gmail.com>
2019-10-06 17:37:24 -0400

    Footer and Nav rendering

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import Portfolio from "./components/body/Portfolio";
+import Nav from "./components/navigation/Nav";
+import Footer from "./components/footer/Footer";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <Nav />
       <Portfolio />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/navigation/Nav.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default () => {
+  return (
+    <div>
+      <div>It's a navbar</div>
+    </div>
+  );
+};
