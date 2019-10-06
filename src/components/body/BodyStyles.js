689ab2d5f4d1afb2a67f92a4d84cbdd5e5caf42a
Rory Murray <rory.christian.murray@gmail.com>
2019-10-06 18:24:39 -0400

    Add makeStyles hook to Headband

---
+++ src/components/body/BodyStyles.js
@@ -0,0 +1,7 @@
+import { makeStyles } from "@material-ui/styles";
+
+export const headbandStyles = makeStyles({
+  headband: {
+    border: "1px solid red"
+  }
+});
--- src/components/body/Headband.js
+++ src/components/body/Headband.js
@@ -1,9 +1,11 @@
 import React from "react";
 import { Grid, Paper } from "@material-ui/core";
+import { headbandStyles } from "./BodyStyles";
 
 export default () => {
+  const styles = headbandStyles();
   return (
-    <Grid container justify="center">
+    <Grid container justify="center" className={styles.headband}>
       <Grid item>Headband active</Grid>
     </Grid>
   );
