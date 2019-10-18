0a36943b9e757eeb2786d5e4f795aaca4e27e377
Rory Murray <rory.christian.murray@gmail.com>
2019-10-18 14:47:01 -0400

    Build Headband style and copy

--- src/components/body/BodyStyles.js
+++ src/components/body/BodyStyles.js
@@ -2,6 +2,13 @@ import { makeStyles } from "@material-ui/styles";
 
 export const headbandStyles = makeStyles({
   headband: {
-    // border: "1px solid red"
+    height: 200,
+    background: "#000000"
+  },
+  headbandcard: {
+    margin: 20
+  },
+  headbandtext: {
+    color: "#F18F01"
   }
 });
--- src/components/body/Headband.js
+++ src/components/body/Headband.js
@@ -5,8 +5,27 @@ import { headbandStyles } from "./BodyStyles";
 export default () => {
   const styles = headbandStyles();
   return (
-    <Grid container justify="center" className={styles.headband}>
-      <Grid item>Headband active</Grid>
+    <Grid
+      container
+      justify="center"
+      direction="column"
+      alignItems="center"
+      className={styles.headband}
+    >
+      <Grid container justify="center" item className={styles.headbandcard}>
+        <Grid item xs={8} className={styles.headbandtext}>
+          Hi. I'm Rory. I'm a Full Stack Human and a Web Developer during about
+          1/2 to 2/3 of the preponderance of my days.
+        </Grid>
+      </Grid>
+      <Grid container justify="center" item className={styles.headbandcard}>
+        <Grid item xs={8} className={styles.headbandtext}>
+          I'm really glad you stopped by. I made this site to speak to and show
+          a bit of what I like to do. If you think it might intersect a bit with
+          what you like to do, or you just wanna know more I'd love to hear from
+          you (about anything, really) here.
+        </Grid>
+      </Grid>
     </Grid>
   );
 };
