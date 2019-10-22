0f435082fbfffada1fcf8b41e770f0c87ea63c2f
Rory Murray <rory.christian.murray@gmail.com>
2019-10-22 16:55:07 -0400

    Add copy

--- src/api/data.js
+++ src/api/data.js
@@ -1,17 +1,20 @@
 export const project = [
   {
     id: 1,
-    title: "Title 1",
-    details: "It was the best of projects, it was the worst of projects"
+    title: "Founder Grants",
+    details:
+      "Completed during Lambda School Labs16 with a team of three other Web Developers and two UX Designers, Founder Grants is a central repository where early-stage founders can search for applicable grants. Utilizes a semi-open curation model."
   },
   {
     id: 2,
-    title: "Title 2",
-    details: "In diss one we worked on diss"
+    title: "Life GPA",
+    details:
+      "Life GPA is an app that lets you track your habits across different categories providing real time score scores."
   },
   {
     id: 3,
-    title: "Title 3",
-    details: "A thing about a thing about a thing"
+    title: "Exercise App",
+    details:
+      "Exercise app is a full CRUD application for users to select and manage exercises based on muscle group"
   }
 ];
