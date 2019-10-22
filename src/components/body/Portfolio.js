334a49c918815168da18ddb27524316c4510a22b
Rory Murray <rory.christian.murray@gmail.com>
2019-10-22 12:32:46 -0400

    Wireframe footer

--- src/components/body/Portfolio.js
+++ src/components/body/Portfolio.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Grid, Paper } from "@material-ui/core";
-import Headband from "./Headband";
+// import Headband from "./Headband";
 import Project from "./Project";
 
 export default () => {
--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,9 +1,27 @@
 import React from "react";
+import { navStyles } from "../styles/navStyles";
+import { Grid } from "@material-ui/core";
+import { Link } from "react-router-dom";
 
 export default () => {
+  const classes = navStyles();
   return (
-    <div>
-      <div>Footer component</div>
-    </div>
+    <Grid container justify="space-around" className={classes.navwrap}>
+      <Link to="#" className={classes.navlink}>
+        <Grid item className={classes.navtext}>
+          Home
+        </Grid>
+      </Link>
+      <Link to="/info" className={classes.navlink}>
+        <Grid item className={classes.navtext}>
+          Info
+        </Grid>
+      </Link>
+      <Link to="/" className={classes.navlink}>
+        <Grid item className={classes.navtext}>
+          Github
+        </Grid>
+      </Link>
+    </Grid>
   );
 };
