10699b8cfd022988f19dee10039945cafd70b11e
Rory Murray <rory.christian.murray@gmail.com>
2019-10-23 19:19:15 -0400

    Create showcase state and simplify setting

--- src/api/data.js
+++ src/api/data.js
@@ -1,24 +1,24 @@
 export const project = [
   {
-    id: 1,
+    id: "foundergrants",
     title: "Founder Grants",
     details:
       "Completed during Lambda School Labs16 with a team of three other Web Developers and two UX Designers, Founder Grants is a central repository where early-stage founders can search for applicable grants. Utilizes a semi-open curation model."
   },
   {
-    id: 2,
+    id: "lifegpa",
     title: "Life GPA",
     details:
       "Life GPA is an app that lets you track your habits across different categories providing real time scores."
   },
   {
-    id: 3,
+    id: "immunizationtracker",
     title: "Immunization Tracker - Java Back End",
     details:
       "I built the back end in Java for the Immunization Tracker app. This includes over 30 endpoints with full CRUD functionality, error handling, docs in Swagger, and authorization utilizing 0Auth2."
   },
   {
-    id: 4,
+    id: "exerciseapp",
     title: "Exercise App",
     details:
       "Exercise app is a full CRUD application for users to select and manage exercises based on muscle group"
--- src/components/body/ProjectShowcase.js
+++ src/components/body/ProjectShowcase.js
@@ -1,14 +1,49 @@
 import React from "react";
-import { Grid } from "@material-ui/core";
+import { project } from "../../api/data";
+import { Grid, Typography, Button } from "@material-ui/core";
+import { portfolioStyles } from "../styles/bodyStyles";
 
 export default props => {
   const [id, setId] = React.useState(props.location.pathname.slice(9));
+  const styles = portfolioStyles();
   console.log("ProjectShowcase props", props);
   console.log("ProjectShowcase id", id);
+  console.log("ProjectShowcase project", project);
+
+  //   const thisProj = project.filter(project => project.id === id);
+  //   console.log("ProjectShowcase thisProj[0]", thisProj[0]);
+  const [showcase, setShowcase] = React.useState(
+    project.filter(project => project.id === id)[0]
+  );
+
+  //   setShowcase(thisProj[0]);
+
+  //   setShowcase({
+  //     id: thisProj[0].id,
+  //     title: thisProj[0].title,
+  //     details: thisProj[0].details
+  //   });
+  console.log("ProjectShowcase showcase", showcase);
 
   return (
-    <Grid container>
-      <Grid item>Project details incoming</Grid>
+    <Grid
+      container
+      item
+      direction="column"
+      justify="center"
+      alignItems="center"
+      className={styles.project}
+    >
+      <Grid item xs={8} className={styles.title}>
+        <Typography variant="subtitle1" className={styles.projectTitleText}>
+          {/* {title} */}derp
+        </Typography>
+      </Grid>
+      <Grid item xs={8} className={styles.projDetails}>
+        <Typography variant="subtitle1" className={styles.projectDetailsText}>
+          {/* {details} */}
+        </Typography>
+      </Grid>
     </Grid>
   );
 };
