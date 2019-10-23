0b17039002c8c0131ff261541b1add89a956ed79
Rory Murray <rory.christian.murray@gmail.com>
2019-10-23 18:56:22 -0400

    Consolidate code

--- src/components/body/ProjectShowcase.js
+++ src/components/body/ProjectShowcase.js
@@ -2,21 +2,9 @@ import React from "react";
 import { Grid } from "@material-ui/core";
 
 export default props => {
-  const [id, setId] = React.useState(0);
-  //   console.log("ProjectShowcase props", props);
-  //   console.log(
-  //     "ProjectShowcase props.location.pathname",
-  //     props.location.pathname
-  //   );
-  //   console.log("ProjectShowcase id", id);
-
-  const regCheck = new RegExp("check", "gi");
-  console.log("ProjectShowcase regCheck", regCheck);
-
-  // slice string
-
-  let loc = props.location.pathname.slice(9);
-  console.log("ProjectShowcase loc", loc);
+  const [id, setId] = React.useState(props.location.pathname.slice(9));
+  console.log("ProjectShowcase props", props);
+  console.log("ProjectShowcase id", id);
 
   return (
     <Grid container>
